48e50eb60cb8404a4ca892847a2294aea2b0e1f2
Chandrashekarh666s <chandrashekar.hv666ts@gmail.com>
2025-05-13 19:07:50 +0530

    Create Simple_interest.c

---
+++ Simple_interest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float p,r,t,SI;
+printf("Enter the values of p ,r ,t");
+scanf("%f%f%f",&p,&r,&t);
+SI=(p*r*t)/100;
+printf("Simple interest=%f",SI);
+return 0;
+}
