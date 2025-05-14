6cfcdb5bb9d148ee0951e9b6c6e2adb7a9c17534
Chandrashekarh666s <chandrashekar.hv666ts@gmail.com>
2025-05-14 09:36:32 +0530

    Create Function_add.c

---
+++ Function_add.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int c;
+int add(int a ,int b){  // function decleration
+    c=a+b; //function definition
+    return c;
+}
+     int main()
+     {
+      int result=add(660,6); //function call
+      printf("%d",c);
+      return 0;
+     }
