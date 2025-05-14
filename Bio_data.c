72807f2fa800da6dcd9390e25feb9612c9ea4701
Chandrashekarh666s <chandrashekar.hv666ts@gmail.com>
2025-05-14 08:14:49 +0530

    Create Bio_data.c

---
+++ Bio_data.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+char name[20];
+int age;
+printf("Enter your Name;\n");
+scanf("%s",&name);
+printf("Enter your Age:\n");
+scanf("%d",&age);
+printf("Your Name is %s, and your age is %d.",name,age);
+return 0;a
+}
