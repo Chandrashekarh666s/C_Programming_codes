36c7213adad27c10f0f9ec4f9e086ae2849686cf
Chandrashekarh666s <chandrashekar.hv666ts@gmail.com>
2025-05-14 08:13:09 +0530

    Create Integer_floatingvalues.c

---
+++ Integer_floatingvalues.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int a;
+float b;
+printf("Enter the values of a and b\n");
+scanf("%d",&a);
+scanf("%f",&b);
+printf("The integer value is %d, and the floating point value is %.3f",a,b);
+    
+    return 0;
+}
